4f47e3a22370c050c24a480e66b00bbff35aa8c8
Elodie Lescuyer <e.lescuyer@touloisirs.fr>
2024-11-18 14:24:43 +0100

    build: add rector and php-cs-fixer

--- rector.php
+++ rector.php
@@ -6,12 +6,12 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/Context',
-        __DIR__ . '/Event',
-        __DIR__ . '/Extractor',
-        __DIR__ . '/Loader',
-        __DIR__ . '/Transformer',
-        __DIR__ . '/Workflow',
+        __DIR__.'/Context',
+        __DIR__.'/Event',
+        __DIR__.'/Extractor',
+        __DIR__.'/Loader',
+        __DIR__.'/Transformer',
+        __DIR__.'/Workflow',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
