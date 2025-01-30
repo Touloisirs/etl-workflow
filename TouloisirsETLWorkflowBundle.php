125874890b2548560144e0eb9259799418d67e4c
Elodie Lescuyer <e.lescuyer@touloisirs.fr>
2025-01-30 10:35:14 +0100

    config

--- TouloisirsETLWorkflowBundle.php
+++ TouloisirsETLWorkflowBundle.php
@@ -2,6 +2,8 @@
 
 namespace Touloisirs\ETLWorkflow;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -11,7 +13,9 @@ class TouloisirsETLWorkflowBundle extends AbstractBundle
     /** @param array<mixed> $config */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        // load an XML, PHP or YAML file
-        $container->import('./config/services.yaml');
+        $container->services()
+            ->set(Client::class)
+            ->alias(ClientInterface::class, Client::class)
+        ;
     }
 }
